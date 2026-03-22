ea71487aff93444ef07548e4aa4aedaf049b82fd
brennoms <brennoms.dev@gmail.com>
2026-03-22 17:25:11 -0300

    fix(ItemList): adjust button positioning and styling; update FlatList class

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -14,19 +14,18 @@ export default function ItemList<T>({ items, renderItem, onAdd }: ItemListProps<
     <View className="flex-1">
 
       <FlatList
+      className="z-2"
         data={items}
         keyExtractor={(item: any) => item.id}
         renderItem={({ item }) => renderItem(item)}
       />
 
-      <View className="absolute bottom-[100px] right-[100px]">
         <TouchableOpacity
           onPress={onAdd}
-          className="w-16 h-16 bg-blue-500 rounded-full justify-center items-center shadow-lg"
+          className="absolute bottom-6 right-6 w-16 h-16 bg-blue-500 rounded-full justify-center items-center shadow-xl elevation-5"
         >
           <Text className="text-white text-3xl font-bold"> + </Text>
         </TouchableOpacity>
-      </View>
 
     </View>
 
