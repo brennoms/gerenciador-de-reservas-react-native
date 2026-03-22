9351d2db589073838e4788959d4822d8d1a87724
brennoms <brennoms.dev@gmail.com>
2026-03-22 20:25:42 -0300

    feat(PropertyShow): create PropertyShow component for displaying property details feat(Index): integrate PropertyShow and add calendar functionality fix(package): add react-native-calendars dependency

---
+++ app/property/[id]/index.tsx
@@ -0,0 +1,81 @@
+import { View, Text, TouchableOpacity, ScrollView } from "react-native"
+import { useState } from "react";
+import AntDesign from '@expo/vector-icons/AntDesign';
+import { Calendar } from "react-native-calendars";
+
+import { useProperty } from "@/src/contexts/property/PropertyHook";
+import PropertyShow from "@/src/components/PropertyShow";
+
+
+
+export default function Index() {
+
+  const today = new Date()
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const { propertySelected } = useProperty()
+
+  const propertyEdit = () => {
+    console.log("edit")
+  }
+
+  return propertySelected ? 
+    (
+
+      <ScrollView className="flex-1 p-4">
+
+        <View className="w-min h-min">
+          <PropertyShow
+            id={propertySelected.id}
+            image={propertySelected.image}
+            name={propertySelected.name}
+            address={propertySelected.address}
+          />
+
+          <TouchableOpacity className="absolute bottom-6 right-3 px-2 rounded-lg bg-blue-100" onPress={() => propertyEdit()}>
+            <AntDesign name="edit" size={32} color="black" />
+          </TouchableOpacity>
+        </View>
+
+        <View className="w-full h-min mb-4">
+          <Calendar
+            style={{ borderRadius: 10 }}
+            enableSwipeMonths={true}
+            hideExtraDays={false}
+            
+            onMonthChange={(month) =>{
+              console.log('month', month)
+            }}
+            onDayPress={(day) => {
+              console.log('selected day', day);
+              const [ano, mes, dia] = day.dateString.split("-").map(Number);
+              setSelectedDate(new Date(ano, mes - 1, dia));
+            }}
+            markedDates={{
+              [today.toLocaleDateString('pt-BR').split('/').reverse().join('-')]: { marked: true, dotColor: 'blue', selectedColor: 'blue' },
+              [selectedDate.toLocaleDateString('pt-BR').split('/').reverse().join('-')]: { selected: true, selectedColor: '#27d3f5' },
+            }}
+
+          />
+        </View>
+
+        <View className="w-full h-min items-center">
+
+            <Text className="text-2xl">Data Selecionada:</Text>
+            <Text className="text-xl color-gray-500">{selectedDate.toLocaleDateString('pt-BR')}</Text>
+
+
+
+        </View>
+
+        <View className="h-40"></View>
+        
+      </ScrollView>
+
+    ) 
+      : 
+    (
+      <View></View>
+    );
+
+}
---
+++ src/components/PropertyShow.tsx
@@ -0,0 +1,24 @@
+import { View, Text, Image } from "react-native";
+
+import { PropertyProps } from "../types/property.types";
+
+
+export default function PropertyShow({ id, image, name, address, className }: PropertyProps & { className?: string }) {
+  return (
+    <View 
+      className={className ? className : "bg-white rounded-xl mb-4 overflow-hidden"}
+    >
+
+      <Image
+        source={{ uri: image }}
+        className="w-full aspect-[1.5]"
+      />
+
+      <View className="p-4 justify-center items-center">
+        <Text className="font-bold text-lg">{name}</Text>
+        <Text className="text-gray-500">{address}</Text>
+      </View>
+
+    </View>
+  );
+}
