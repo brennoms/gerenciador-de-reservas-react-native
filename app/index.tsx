af5698f9b0e09e81fbd6e054425bf0ee79991c71
brennoms <brennoms.dev@gmail.com>
2026-03-28 17:25:20 -0300

    feat: implement server connection check with loading indicator

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,40 @@
 import { Redirect } from "expo-router";
-import { useEffect } from "react";
-import axios from "axios"
+import { useEffect, useState } from "react";
+import { ActivityIndicator, View, Text } from "react-native";
+import axios from "axios";
 
 export default function Index() {
+  const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
-    axios.get(`${process.env.EXPO_PUBLIC_API_URL}`)
-    .then((data) => {
-      console.log(data.data);
-    })
-  }, [])
+
+    const interval = setInterval(async () => {
+      try {
+        const res = await axios.get(`${process.env.EXPO_PUBLIC_API_URL}`);
+
+        if (res.status === 200) {
+          setLoaded(true);
+          clearInterval(interval);
+        }
+      } catch (error) {
+        console.log("Servidor offline", error);
+      }
+    }, 2000);
+
+    return () => clearInterval(interval);
+
+  }, []);
+
+  if (loaded) {
+    return <Redirect href="/login" />;
+  }
 
   return (
-    <Redirect href="/login"/>
+    <View className="flex-1 items-center justify-center">
+      <ActivityIndicator size="large" color="#3b82f6" />
+      <Text className="text-[#3b82f6]">
+        Conectando ao servidor...
+      </Text>
+    </View>
   );
 }
