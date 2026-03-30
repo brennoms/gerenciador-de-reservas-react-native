459e740b6de4563dbf49c025607d72b673f5238e
Brenno Marques <brennoms.dev@gmail.com>
2026-03-30 14:33:25 -0300

    feat(calendar): merge useEffect deps and improve day styling

--- src/contexts/calendar/CalendarProvider.tsx
+++ src/contexts/calendar/CalendarProvider.tsx
@@ -17,13 +17,7 @@ export function CalendarProvider({ children }: { children: ReactNode }) {
 
     reloadStyledDays();
 
-  }, [reservations])
-
-  useEffect(() => {
-
-    reloadStyledDays();
-
-  }, [selectedDate])
+  }, [reservations, selectedDate])
 
   const calendarDayPress = (day: Date) => {
     setSelectedDate(day);
@@ -34,6 +28,11 @@ export function CalendarProvider({ children }: { children: ReactNode }) {
   }
 
   const reloadStyledDays = () => {
+
+    function isoDate(date: Date) {
+      return date.toISOString().split("T")[0];
+    }
+
     const newStyledDays = {};
 
     for (const reservation of reservations) {
@@ -42,23 +41,23 @@ export function CalendarProvider({ children }: { children: ReactNode }) {
       if (reservation.id === selectedReservation?.id){
         color = "orange";
       } else {
-        color = "blue";
+        color = "darkblue";
       }
 
-      newStyledDays[reservation.init_date.toISOString().split("T")[0]] = {
+      newStyledDays[isoDate(reservation.init_date)] = {
         startingDay: true,
         color: color,
         textColor: "white"
       }
       
       for (const i = new Date(reservation.init_date.getTime() + (24 * 60 * 60 * 1000)) ; i < reservation.end_date ; i.setDate(i.getDate() + 1)) {
-        newStyledDays[i.toISOString().split("T")[0]] = {
+        newStyledDays[isoDate(i)] = {
           color: color,
           textColor: "white"
         }
       }
 
-      newStyledDays[reservation.end_date.toISOString().split("T")[0]] = {
+      newStyledDays[isoDate(reservation.end_date)] = {
         endingDay: true,
         color: color,
         textColor: "white"
@@ -66,13 +65,27 @@ export function CalendarProvider({ children }: { children: ReactNode }) {
 
     }
 
-    if (newStyledDays[today.toISOString().split("T")[0]]) {
-      newStyledDays[today.toISOString().split("T")[0]].color = "lightblue"
-      newStyledDays[today.toISOString().split("T")[0]].textColor = "black"
+    if (newStyledDays[isoDate(today)]) {
+      newStyledDays[isoDate(today)].color = "blue"
+      newStyledDays[isoDate(today)].textColor = "white"
     } else {
-      newStyledDays[today.toISOString().split("T")[0]] = {
-        color: "lightblue",
-        textColor: "black"
+      newStyledDays[isoDate(today)] = {
+        color: "blue",
+        textColor: "white",
+        startingDay: true,
+        endingDay: true
+      }
+    }
+
+    if (newStyledDays[isoDate(selectedDate)]) {
+      newStyledDays[isoDate(selectedDate)].color = `dark${newStyledDays[isoDate(selectedDate)].color}`
+      newStyledDays[isoDate(selectedDate)].textColor = "white"
+    } else {
+      newStyledDays[isoDate(selectedDate)] = {
+        color: "gray",
+        textColor: "white",
+        startingDay: true,
+        endingDay: true
       }
     }
 
