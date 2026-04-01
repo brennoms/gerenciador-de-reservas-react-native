6743d7a49f33d10aa00c7da25549cc33587a93a0
Brenno Marques <brennoms.dev@gmail.com>
2026-04-01 18:11:48 -0300

    feat(calendar): add Portuguese (pt-br) locale configuration

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,5 +1,24 @@
 import React from "react";
-import { Calendar as RNCalendar } from "react-native-calendars";
+import { Calendar as RNCalendar, LocaleConfig } from "react-native-calendars";
+
+LocaleConfig.locales['pt-br'] = {
+  monthNames: [
+    'Janeiro','Fevereiro','Março','Abril','Maio','Junho',
+    'Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'
+  ],
+  monthNamesShort: [
+    'Jan','Fev','Mar','Abr','Mai','Jun',
+    'Jul','Ago','Set','Out','Nov','Dez'
+  ],
+  dayNames: [
+    'Domingo','Segunda','Terça','Quarta','Quinta','Sexta','Sábado'
+  ],
+  dayNamesShort: [
+    'Dom','Seg','Ter','Qua','Qui','Sex','Sáb'
+  ]
+};
+
+LocaleConfig.defaultLocale = 'pt-br';
 
 type CalendarProps = {
   current: Date;
